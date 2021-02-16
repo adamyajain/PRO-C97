36fd79660da1349837a80f0fde68aa6393e2ac3a
adamyajain <apoorvadoc@gmail.com>
2021-02-16 18:57:04 +0530

    commited

---
+++ code.py
@@ -0,0 +1,8 @@
+age  = int(input("Enter Your Age: "))
+
+if(age>18):
+    print("You are an adult. You can Vote!")
+elif(age>12):
+    print("You are a teenager and a rebel!")
+else:
+    print("You are still a kid There is still so much for you to see")
---
+++ countingWords.py
@@ -0,0 +1,12 @@
+introString = input("Enter String: ")
+charCount = 0
+wordCount = 1
+
+for i in introString:
+    charCount = charCount+1
+    if(i==' '):
+        wordCount = wordCount+1
+print("Number Of Words in a String: ")
+print(wordCount)
+print("Number Of Characters in a String: ")
+print(charCount)
